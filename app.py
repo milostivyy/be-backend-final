86d0e33271d209ed3a18ff0b11e6eb2d4954dad4
Mahima Chauhan <mahima19197@gmail.com>
2023-05-21 17:59:29 +0530

    added

--- app.py
+++ app.py
@@ -25,10 +25,11 @@ def submit_data():
     input_array= request.get_json()
     input_array = np.array(input_array)
     print("array is",input_array)
+    input_array = [text.replace('"', '') for text in input_array if text != '""']
     #print("input array",input_array)
 
     #Perform sentiment analysis and store the results in a list
-    #sentiment_scores = [1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1]
+    #sentiment_scores = [1,-1,1,1,0,0,1,0,-1,0,1,0,1,-1,1,1]
     sentiment_scores=[]
     for text in input_array:
         score = sid.polarity_scores(str(text))
@@ -38,9 +39,11 @@ def submit_data():
             sentiment_scores.append(0)
         else:
             sentiment_scores.append(-1)
+    #print("sentiment socres",sentiment_scores)
     sentiment_scores=np.array(sentiment_scores)
-    reshaped_array = sentiment_scores.reshape(1, -1)
-    
+    #print("sentiment scores after np is:",sentiment_scores)
+    reshaped_array = [sentiment_scores]
+    #print("reshape array",reshaped_array)
     predictions = svm_model.predict(reshaped_array)
     disorder = predictions[0]
 
@@ -48,7 +51,14 @@ def submit_data():
     # # # process the data here
     # data = {'name': 'John', 'age': 30, 'city': 'New York'}
     # eturn the processed data as JSON
-    #print("disorder",disorder)
-    return jsonify({'You are suffering from': disorder})
+    #print("disorder",disorde
+    print_statement = "If you or someone you know is suffering from anxiety and mood disorders, here are some suggestions and feedback to help manage and cope with these conditions:\n" \
+                      "1. Seek professional help: It's crucial to consult with a mental health professional such as a psychologist or psychiatrist. They can provide an accurate diagnosis and develop an appropriate treatment plan tailored to your needs.\n" \
+                      "2. Educate yourself: Learn about anxiety and mood disorders to better understand what you're experiencing. Knowledge can help alleviate some of the fear and uncertainty surrounding these conditions.\n" \
+                      "3. Build a support system: Surround yourself with understanding and supportive individuals who can offer encouragement and empathy. Consider joining support groups or seeking therapy to connect with others who share similar experiences.\n" \
+                      "4. Practice self-care: Prioritize self-care activities that promote physical and mental well-being. Engage in regular exercise, eat a balanced diet, get enough sleep, and engage in activities you enjoy."
+    response= jsonify({'You are suffering from': disorder,"Suggestion":print_statement})
+    return response
+    # return response
 if __name__ == '__main__':
     app.run(debug=True)
